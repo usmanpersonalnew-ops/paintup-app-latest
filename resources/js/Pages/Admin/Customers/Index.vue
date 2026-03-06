7a4181926e75e334ce0bee4830f6551069a754c7
Pankaj <144772128+bnsinfo@users.noreply.github.com>
2026-03-07 00:54:25 +0530

    updated

---
+++ resources/js/Pages/Admin/Customers/Index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+</script>
+
+<template>
+    <AdminLayout>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Customers
+        </h2>
+    </AdminLayout>
+</template>
